1e74d97cb4781a22583ffc59aad66c3685243a51
Abdulrahman Aldayhani <148686321+Ab700700@users.noreply.github.com>
2023-12-14 18:03:41 +0300

    Delete SchoolManagement directory

--- SchoolManagement/src/main/java/com/example/schoolmanagement/Repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.schoolmanagement.Repository;
-
-import com.example.schoolmanagement.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher,Integer> {
-
-    Teacher findTeacherById(Integer id);
-}
